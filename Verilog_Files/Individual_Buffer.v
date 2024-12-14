25e75df352235b2686dcf0f29fc31fd9d2de76fb
Ajay Sudharshan Satish <ajaysatish3801@gmail.com>
2024-12-14 16:20:40 -0500

    Completed and Verified banked buffers

--- Verilog_Files/Individual_Buffer.v
+++ Verilog_Files/Individual_Buffer.v
@@ -1,13 +1,13 @@
 `timescale 1ns/1ps
 
-module indivudual_buffer #(
+module individual_buffer #(
     parameter ARR_SIZE = 4 
 )(
     input clk,
     input rst,
     input [15:0] individual_input,    //64-bit input from the external interface
     input [1:0] state,
-    output reg [15:0] individual_output, //64-bit output to the controller
+    output reg [15:0] individual_output //64-bit output to the controller
 );
     
     parameter QUEUE_DEPTH = ARR_SIZE*2; //Maximum number of instructions in the queue
@@ -29,13 +29,13 @@ module indivudual_buffer #(
     always @(posedge clk) begin
 
         // Enqueue - Load instruction from interface to the queue
-        if (state=2'b01) begin
+        if (state == 2'b01) begin
             queue[tail] <= individual_input;  // Load instruction to the queue
             tail <= (tail + 1) % QUEUE_DEPTH; // Increment tail pointer (go back to 0 when its the end, stay in the range 0 to QUEUE_DEPTH-1)
             count <= count + 1;             // Increment instruction count
         end
         
-        else if (state=2'b10) begin // Dequeue - Send instruction to the controller
+        else if (state == 2'b10) begin // Dequeue - Send instruction to the controller
             individual_output <= queue[head]; // Send instruction to controller
             head <= (head + 1) % QUEUE_DEPTH;   // Increment head pointer (refer comment for tail)
             count <= count - 1;               // Decrement instruction count
