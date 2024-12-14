53b78dd02edb28c5c797c6ddb1d9b34f2ded9e1d
Aishwarya-Kandam-Vitta <aishwarya.kandam@gmail.com>
2024-12-14 17:29:57 -0500

    Correction - failed to generate full power grid - trial 5

--- Logic Synthesis/pdn.tcl
+++ Logic Synthesis/pdn.tcl
@@ -11,20 +11,24 @@ set_voltage_domain -name {CORE} -power {VDD} -ground {VSS}
 define_pdn_grid -name {grid} -voltage_domains {CORE}
 
 # Metal1 Stripes
-add_pdn_stripe -grid {grid} -layer {met1} -width {0.49} -pitch {5.44}
+add_pdn_stripe -grid {grid} -layer {met1} -width {0.49} -pitch {4.32}
 
 # Metal3 Stripes
-add_pdn_stripe -grid {grid} -layer {met3} -width {1.20} -pitch {20.0} -offset {2}
+add_pdn_stripe -grid {grid} -layer {met3} -width {1.20} -pitch {20.0} -offset {0.5}
 
 # Metal4 Stripes
-add_pdn_stripe -grid {grid} -layer {met4} -width {0.96} -pitch {20.0} -offset {2} -extend_to_core_ring
+add_pdn_stripe -grid {grid} -layer {met4} -width {0.96} -pitch {20.0} -offset {0.5} -extend_to_core_ring
 
 # Metal5 Stripes
-add_pdn_stripe -grid {grid} -layer {met5} -width {1.60} -pitch {25.0} -offset {2} -extend_to_core_ring
+add_pdn_stripe -grid {grid} -layer {met5} -width {1.60} -pitch {25.0} -offset {0.5} -extend_to_core_ring
 
-# Adjusted Power Rings (with smaller offsets to fit within the die)
-add_pdn_ring -grid {grid} -layers {met4 met5} -widths {2 2} -spacings {1.6 1.6} \
-    -core_offsets {2 2 2 2} -connect_to_pads
+# Adding Power Rings
+add_pdn_ring -grid {grid} -layers {met4 met5} -widths {1.5 1.5} -spacings {1.0 1.0} \
+    -core_offsets {1.0 1.0 1.0 1.0} -connect_to_pads
+
+# Layer Connections
+add_pdn_connect -grid {grid} -layers {met3 met4}
+add_pdn_connect -grid {grid} -layers {met4 met5}
 
 # Macro grids
 define_pdn_grid -name {CORE_macro_grid} -voltage_domains {CORE} -macro \
