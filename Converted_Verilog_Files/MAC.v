03e8667149db818bf0ac64538b3aa5b799373e72
Ajay Sudharshan Satish <ajaysatish3801@gmail.com>
2024-12-16 00:48:54 -0500

    Fixed MAC; not sure if the fix works

--- Converted_Verilog_Files/MAC.v
+++ Converted_Verilog_Files/MAC.v
@@ -1,99 +1,104 @@
-module MAC (
-	clk,
-	i_mode,
-	rst,
-	vertical_input,
-	horizontal_input,
-	MAC_OP
+`timescale 1ns/1ps
+
+
+module MAC #(
+    parameter ARR_SIZE = 4, // set to 128 during final synthesis 
+    parameter VERTICAL_BW = 32,
+    parameter HORIZONTAL_BW = 16,
+    parameter ACC_SIZE = 64
+) (
+    input clk,
+    input i_mode,
+    input rst,
+    input [HORIZONTAL_BW * ARR_SIZE - 1 : 0] vertical_input,
+    input [HORIZONTAL_BW * ARR_SIZE - 1 : 0] horizontal_input,
+    output reg [ARR_SIZE * VERTICAL_BW - 1 : 0] MAC_OP
 );
-	parameter ARR_SIZE = 4;
-	parameter VERTICAL_BW = 32;
-	parameter HORIZONTAL_BW = 16;
-	parameter ACC_SIZE = 64;
-	input clk;
-	input i_mode;
-	input rst;
-	input [(HORIZONTAL_BW * ARR_SIZE) - 1:0] vertical_input;
-	input [(HORIZONTAL_BW * ARR_SIZE) - 1:0] horizontal_input;
-	output reg [(ARR_SIZE * VERTICAL_BW) - 1:0] MAC_OP;
-	wire [HORIZONTAL_BW - 1:0] horizontal_wires [ARR_SIZE - 1:0][ARR_SIZE - 1:0];
-	wire [VERTICAL_BW - 1:0] vertical_wires [ARR_SIZE - 1:0][ARR_SIZE - 1:0];
-	wire [(ARR_SIZE * VERTICAL_BW) - 1:0] op_wire;
-	genvar _gv_i_1;
-	genvar _gv_j_1;
-	generate
-		for (_gv_i_1 = 0; _gv_i_1 < ARR_SIZE; _gv_i_1 = _gv_i_1 + 1) begin : genblk1
-			localparam i = _gv_i_1;
-			for (_gv_j_1 = 0; _gv_j_1 < ARR_SIZE; _gv_j_1 = _gv_j_1 + 1) begin : genblk1
-				localparam j = _gv_j_1;
-				if ((i == 0) && (j == 0)) begin : genblk1
-					PE pe_instance_tlcorner(
-						.clk(clk),
-						.rst(rst),
-						.i_mode(i_mode),
-						.i_top({16'b0000000000000000, vertical_input[((j + 1) * HORIZONTAL_BW) - 1:j * HORIZONTAL_BW]}),
-						.i_left(horizontal_input[((i + 1) * HORIZONTAL_BW) - 1:i * HORIZONTAL_BW]),
-						.o_bot(vertical_wires[i][j]),
-						.o_right(horizontal_wires[i][j])
-					);
-				end
-				else if ((i == 0) && (j != 0)) begin : genblk1
-					PE pe_instance_top(
-						.clk(clk),
-						.rst(rst),
-						.i_mode(i_mode),
-						.i_top({16'b0000000000000000, vertical_input[((j + 1) * HORIZONTAL_BW) - 1:j * HORIZONTAL_BW]}),
-						.i_left(horizontal_wires[i][j - 1]),
-						.o_bot(vertical_wires[i][j]),
-						.o_right(horizontal_wires[i][j])
-					);
-				end
-				else if ((i != 0) && (j == 0)) begin : genblk1
-					PE pe_instance_left(
-						.clk(clk),
-						.rst(rst),
-						.i_mode(i_mode),
-						.i_top(vertical_wires[i - 1][j]),
-						.i_left(horizontal_input[((i + 1) * HORIZONTAL_BW) - 1:i * HORIZONTAL_BW]),
-						.o_bot(vertical_wires[i][j]),
-						.o_right(horizontal_wires[i][j])
-					);
-				end
-				else if ((i == (ARR_SIZE - 1)) && (j == 0)) begin : genblk1
-					PE pe_instance_blcorner(
-						.clk(clk),
-						.rst(rst),
-						.i_mode(i_mode),
-						.i_top(vertical_wires[i - 1][j]),
-						.i_left(horizontal_input[((i + 1) * HORIZONTAL_BW) - 1:i * HORIZONTAL_BW]),
-						.o_bot(op_wire[(j + 1) * VERTICAL_BW:j * VERTICAL_BW]),
-						.o_right(horizontal_wires[i][j])
-					);
-				end
-				else if ((i == (ARR_SIZE - 1)) && (j != 0)) begin : genblk1
-					PE pe_instance_bottom(
-						.clk(clk),
-						.rst(rst),
-						.i_mode(i_mode),
-						.i_top(vertical_wires[i - 1][j]),
-						.i_left(horizontal_wires[i][j - 1]),
-						.o_bot(op_wire[(j + 1) * VERTICAL_BW_BW:j * VERTICAL_BW_BW]),
-						.o_right(horizontal_wires[i][j])
-					);
-				end
-				else if ((i != 0) && (j != 0)) begin : genblk1
-					PE pe_instance_middle(
-						.clk(clk),
-						.rst(rst),
-						.i_mode(i_mode),
-						.i_top(vertical_wires[i - 1][j]),
-						.i_left(horizontal_wires[i][j - 1]),
-						.o_bot(vertical_wires[i][j]),
-						.o_right(horizontal_wires[i][j])
-					);
-				end
-			end
-		end
-	endgenerate
-	always @(posedge clk) MAC_OP <= op_wire;
+
+    wire [HORIZONTAL_BW-1:0]horizontal_wires[ARR_SIZE-1:0][ARR_SIZE-1:0];
+    wire [VERTICAL_BW-1:0]vertical_wires[ARR_SIZE-1:0][ARR_SIZE-1:0];
+    wire [ARR_SIZE * VERTICAL_BW - 1 : 0] op_wire;
+
+    generate
+        genvar i;
+        genvar j;
+        genvar k;
+        for (i=0; i<ARR_SIZE; i=i+1) begin // generates rows of elements
+            for (j=0; j<ARR_SIZE; j=j+1) begin // generate each row of elements
+                
+                // Types of elements
+                //  1) Corner Elements:
+                //      1) Top-left corner
+                //      2) Top-right corner - handled in top row scenario
+                //      3) Bottom-left corner - handled in bottom row scenario
+                //      4) Bottom right corner - handled in bottom row scenario
+                //  2) Edge elements
+                //      1) 1st row of elements
+                //      2) 1st column of elements
+                //      3) last row of elements - handled by other elements
+                //      4) last column of elements  - handled by other elements
+                //  3) other elements
+                // total of 4 different types of elements to be coded
+
+
+                if(i==0 && j==0) begin //top-left corner element
+                    PE pe_instance(
+                        .clk(clk),
+                        .rst(rst),
+                        .i_mode(i_mode),
+                        .i_top({16'b0,vertical_input[ (j+1) * HORIZONTAL_BW - 1 : j * HORIZONTAL_BW]}),
+                        .i_left(horizontal_input[ (i+1) * HORIZONTAL_BW - 1 : i * HORIZONTAL_BW]),
+                        .o_bot(vertical_wires[i][j]),
+                        .o_right(horizontal_wires[i][j])
+                    );
+
+                end
+
+                if(i==0 && j!=0) begin //Top elements
+                    PE pe_instance(
+                        .clk(clk),
+                        .rst(rst),
+                        .i_mode(i_mode),
+                        .i_top({16'b0,vertical_input[(j+1) * HORIZONTAL_BW - 1 : j * HORIZONTAL_BW]}),
+                        .i_left(horizontal_wires[i][j-1]),
+                        .o_bot(vertical_wires[i][j]),
+                        .o_right(horizontal_wires[i][j])
+                    );
+                end
+
+                if(i!=0 && j==0) begin //Left elements
+                    PE pe_instance(
+                        .clk(clk),
+                        .rst(rst),
+                        .i_mode(i_mode),
+                        .i_top(vertical_wires[i-1][j]),
+                        .i_left(horizontal_input[(i+1) * HORIZONTAL_BW - 1 : i * HORIZONTAL_BW]),
+                        .o_bot(vertical_wires[i][j]),
+                        .o_right(horizontal_wires[i][j])
+                    );
+                end
+
+                if(i!=0 && j!=0) begin //Other elements
+                    PE pe_instance(
+                        .clk(clk),
+                        .rst(rst),
+                        .i_mode(i_mode),
+                        .i_top(vertical_wires[i-1][j]),
+                        .i_left(horizontal_wires[i][j-1]),
+                        .o_bot(vertical_wires[i][j]),
+                        .o_right(horizontal_wires[i][j])
+                    );
+                end
+            end
+        end
+
+        for(k=0; k<ARR_SIZE; k=k+1) begin
+            assign op_wire[(k+1) * VERTICAL_BW - 1 : k * VERTICAL_BW] = vertical_wires[ARR_SIZE-1][k];
+        end
+    endgenerate
+
+    always @(posedge clk) begin
+        MAC_OP <= op_wire;
+    end
+
 endmodule
